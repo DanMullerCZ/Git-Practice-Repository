6034444a846d84e30fa5b59a5e3ee2f842727acb
Jakub Stündl <jakub.stundl@gmail.com>
2022-12-12 11:49:15 +0100

    d6

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 
 console.log("Hello D&D nerds");
 
-const cheer = (name='D&D nerds') => {
-    console.log(name)}
+const cheer = (arg) => {
+    console.log(`Cheer for the ${arg}`)}
 
 (async()=>{const future = (param1,param2) =>{
 console.log(param1,param2)}})
