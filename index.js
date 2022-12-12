759bdcf568b007a1818291657a0c32f24ffddad4
PetrHer <petrherynek@seznam.cz>
2022-12-12 11:35:12 +0100

    c5

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 
 console.log("Hello D&D nerds");
 
-const cheer = (name='D&D nerds') => {
-    console.log(name)}
+const cheer = (name='D&D nerds',team=['kuba','adam','petr','dan','luky']) => {
+    console.log(name,team)}
 
 const future = () =>{
 console.log("My horoscope sign is unknown")}
