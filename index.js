891197abb0e42b761f7d019298900bcacb7933d5
Jakub Stündl <jakub.stundl@gmail.com>
2022-12-12 11:20:31 +0100

    future function changed

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cheer = () => {
     console.log('D&D nerds')}
 
 const future = () =>{
-console.log("My horoscope sign is taurus")}
+console.log("My horoscope sign is unknown")}
 
 function count(myAge) {
     console.log(myAge)
